f0fc63fd0649fb45b835f1bce49d9a1cfe8cce22
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-06-08 00:12:55 +0700

    Add hint

--- game.js
+++ game.js
@@ -3,6 +3,24 @@ let max_num = JSON.parse(localStorage.getItem("max_num"))
 let randomNumber = JSON.parse(localStorage.getItem("randomNumber"))
 
 document.getElementById("check-btn").addEventListener("click", function () {
-    console.log(min_num, max_num)
-    console.log(randomNumber)
+    let guessNumber = parseInt(document.getElementsByClassName("input-number")[0].value)
+    if (!check()) {
+        setHint("Error! please check your input")
+    } else if (guessNumber === randomNumber) {
+        setHint("Correct!")
+    } else if (guessNumber > randomNumber) {
+        setHint("Too high!")
+    } else {
+        setHint("Too Low!")
+    }
 })
+
+function check() {
+    let guessNumber = parseInt(document.getElementsByClassName("input-number")[0].value)
+    if (isNaN(guessNumber)) return false
+    return true
+}
+
+function setHint(text) {
+    document.getElementById("hint").innerText = text
+}
