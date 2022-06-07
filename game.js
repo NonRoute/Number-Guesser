cfc990f1bc7389ffeeecfe6d11e5f755dbbe60b0
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-06-08 00:29:01 +0700

    Add restart button

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
-let min_num = JSON.parse(localStorage.getItem("min_num"))
-let max_num = JSON.parse(localStorage.getItem("max_num"))
+let min_num = parseInt(JSON.parse(localStorage.getItem("min_num")))
+let max_num = parseInt(JSON.parse(localStorage.getItem("max_num")))
 let randomNumber = JSON.parse(localStorage.getItem("randomNumber"))
+
+document.getElementById("possible-number").textContent = `Possible numbers are between ${min_num} to ${max_num}`
 
 document.getElementById("check-btn").addEventListener("click", function () {
     let guessNumber = parseInt(document.getElementsByClassName("input-number")[0].value)
@@ -8,12 +10,16 @@ document.getElementById("check-btn").addEventListener("click", function () {
         setHint("Error! please check your input")
     } else if (guessNumber === randomNumber) {
         setHint("Correct!")
+        showRestartBtn()
     } else if (guessNumber > randomNumber) {
         setHint("Too high!")
+        hideRestartBtn()
     } else {
         setHint("Too Low!")
+        hideRestartBtn()
     }
 })
+
 
 function check() {
     let guessNumber = parseInt(document.getElementsByClassName("input-number")[0].value)
@@ -23,4 +29,17 @@ function check() {
 
 function setHint(text) {
     document.getElementById("hint").innerText = text
+}
+
+function showRestartBtn() {
+    document.getElementById("restart").innerHTML = `
+        <button type="button" id="restart-btn">Restart!</button>
+    `
+    document.getElementById("restart-btn").addEventListener("click", function() {
+        window.location.href = "index.html"
+    })
+}
+
+function hideRestartBtn() {
+    document.getElementById("restart").innerHTML = ""
 }
