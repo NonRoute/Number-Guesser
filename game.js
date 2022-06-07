d6c8e96036904b722d2f729636b16bae98737479
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-06-08 00:05:07 +0700

    Add error warning

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 let min_num = JSON.parse(localStorage.getItem("min_num"))
 let max_num = JSON.parse(localStorage.getItem("max_num"))
-
+let randomNumber = JSON.parse(localStorage.getItem("randomNumber"))
 
 document.getElementById("check-btn").addEventListener("click", function () {
     console.log(min_num, max_num)
+    console.log(randomNumber)
 })
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-document.getElementById("play-btn").addEventListener("click", function() {
-    setMinMax()
+const min_numEl = document.getElementById("min_num")
+const max_numEl = document.getElementById("max_num")
+
+document.getElementById("play-btn").addEventListener("click", function () {
+    if (!check()) {
+        document.getElementById("error").textContent = "Error! please check your input"
+    } else {
+        setMinMax()
+        generateNumber()
+        window.location.href = "game.html"
+    }
 })
 
+function check() {
+    let min = parseInt(min_numEl.value)
+    let max = parseInt(max_numEl.value)
+    if (min > max || isNaN(min) || isNaN(max)) return false
+    return true
+}
 function setMinMax() {
-    localStorage.setItem("min_num", JSON.stringify(document.getElementById("min_num").value))
-    localStorage.setItem("max_num", JSON.stringify(document.getElementById("max_num").value))
+    localStorage.setItem("min_num", JSON.stringify(min_numEl.value))
+    localStorage.setItem("max_num", JSON.stringify(max_numEl.value))
+}
+
+function generateNumber() {
+    let max = parseInt(max_numEl.value)
+    let min = parseInt(min_numEl.value)
+    console.log(min, max)
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+    console.log(randomNumber)
+    localStorage.setItem("randomNumber", JSON.stringify(randomNumber))
 }
